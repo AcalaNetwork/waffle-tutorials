adf0e12e99c13663757d38f07a7e21e4e1adb5f2
Jan Turk <jan.turk12@gmail.com>
2021-12-05 22:32:22 +0100

    Add Token
    Added an ERC20 token tutorial.

---
+++ token/src/deploy.ts
@@ -0,0 +1,31 @@
+import { use } from 'chai';
+import { ContractFactory } from 'ethers';
+
+import { evmChai } from '@acala-network/bodhi';
+
+import Token from '../build/Token.json';
+import setup from './setup';
+
+use(evmChai);
+
+const main = async () => {
+    const { wallet, provider } = await setup();
+
+    console.log('Deploy Token');
+
+    const instance = await ContractFactory.fromSolidity(Token).connect(wallet).deploy(1234567890);
+
+    console.log("Token address:", instance.address);
+
+    const name = await instance.name();
+    const symbol = await instance.symbol();
+    const totalSupply = await instance.totalSupply();
+
+    console.log("Token name:", name);
+    console.log("Token symbol:", symbol);
+    console.log("Token total supply:", totalSupply.toNumber());
+
+    provider.api.disconnect();
+}
+
+main()
