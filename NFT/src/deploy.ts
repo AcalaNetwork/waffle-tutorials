1a8ecc52ad1db1a54a372d67b2effa9408b83176
Jan Turk <jan.turk12@gmail.com>
2021-12-15 23:41:31 +0100

    Add NFT tutorial
    Added NFT tutorial to the collection.

---
+++ NFT/src/deploy.ts
@@ -0,0 +1,34 @@
+import { use } from 'chai';
+import { ContractFactory } from 'ethers';
+
+import { evmChai } from '@acala-network/bodhi';
+
+import NFT from '../build/NFT.json';
+import setup from './setup';
+
+use(evmChai);
+
+const main = async () => {
+    const { wallet, provider } = await setup();
+
+    console.log('Deploy NFT');
+
+    const instance = await ContractFactory.fromSolidity(NFT).connect(wallet).deploy();
+
+    console.log("NFT address:", instance.address);
+
+    const name = await instance.name();
+    const symbol = await instance.symbol();
+
+    await instance.mintNFT(await wallet.getAddress(), "exclusive-first-token");
+
+    const URI = await instance.tokenURI(1);
+
+    console.log("NFT name:", name);
+    console.log("NFT symbol:", symbol);
+    console.log("Minted NFT URI:", URI);
+
+    provider.api.disconnect();
+}
+
+main()
